c370b4cdb72e12cf6a7720315ff92ba439187ba2
jaykay12 <jalazkumar1208@gmail.com>
2016-11-05 16:07:38 +0530

    Added supporting codes

---
+++ Supporting Codes/FCFS.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	int queue[20],num,HeadInitial,MaxRange,SeekCylinder=0,diff,i;
+	float AverageSeekTime;
+
+	cout<<"Enter Max Range of Disk: ";
+	cin>>MaxRange;
+	cout<<"Enter Number of Input Sequences(under 20): ";
+	cin>>num;
+
+	cout<<"Enter Input Sequence: ";
+	for(i=1;i<=num;i++)
+		cin>>queue[i];
+
+	cout<<"Enter Initial Head position: ";
+	cin>>HeadInitial;
+
+	queue[0]=HeadInitial;
+
+	for(i=0;i<num;i++)
+	{
+		diff=abs(queue[i+1]-queue[i]);
+		SeekCylinder += diff;
+	}
+
+	AverageSeekTime = SeekCylinder/(float)num;
+
+	cout<<"\n Using FCFS Disc Scheduling";
+	cout<<"\n Total Cylinders : "<<SeekCylinder;
+	cout<<"\n Average Seek time :"<<AverageSeekTime;
+	cout<<"\n";
+return 0;
+}
